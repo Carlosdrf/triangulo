199001df3a8b44ffec8714ed214921f59d7f401a
Carlos <cdavidrivas@hotmail.com>
2021-10-20 20:26:47 -0400

    ajuste

--- ejecutar.php
+++ ejecutar.php
@@ -4,7 +4,7 @@ if(isset($_POST['btn'])&& $_POST['btn']== 'calcular'){
     $lado2 = $_POST['lado2'];
     $hipotenusa = null;
 
-    if(!empty($lado1) or !empty($lado2)){
+    if(!empty($lado1) && !empty($lado2)){
 
         echo "<h2 align='center'>calculo de la hipotenusa de un triangulo</h2>";
 
@@ -13,6 +13,9 @@ if(isset($_POST['btn'])&& $_POST['btn']== 'calcular'){
         echo "<br>La hipotenusa del triangulo es: ".round($hipotenusa,3)."cm²";
         echo "<br/><br/>";
         echo "<a href='index.html'>regresar</a>";
+    }else{
+        echo "<h2 align='center'>Debes completar toda la informacion vuelve a intentarlo</h2>";
+        echo "<p align= 'center'><a  href='index.html'>regresar</a></p>";
     }
 }
 ?>
